844e4fd60ccb4df8c26b1b06e438b94c86317e15
Засыпин Николай <zasipin@mail.ru>
2014-10-25 21:03:21 +0400

    Chapter 14 Initialization

--- controllers.js
+++ controllers.js
@@ -1,16 +1,17 @@
 var app = angular.module("exampleApp", []);
-app.controller("simpleCtrl", function($scope){
-	$scope.buttonEnabled = true;
-	$scope.clickCounter = 0;
-	
-	$scope.handleClick = function(){
-		$scope.clickCounter++;
-	};
-	$scope.$watch('buttonEnabled', function(newValue){
-		$('#jqui button').button({
-			disabled: !newValue
-		});
-	});
-	
+app.controller("defaultCtrl", function($scope){
+	$scope.products = [
+		{ name: "Apples", category: "Fruit", price: 1.20, expiry: 10 },
+		{ name: "Bananas", category: "Fruit", price: 2.42, expiry: 7 },
+		{ name: "Pears", category: "Fruit", price: 2.02, expiry: 6 },
+		
+		{ name: "Tuna", category: "Fish", price: 20.45, expiry: 3 },
+		{ name: "Salmon", category: "Fish", price: 17.93, expiry: 2 },
+		{ name: "Trout", category: "Fish", price: 12.93, expiry: 4 },
+		
+		{ name: "Beer", category: "Drinks", price: 2.99, expiry: 365 },
+		{ name: "Wine", category: "Drinks", price: 8.99, expiry: 365 },
+		{ name: "Whiskey", category: "Drinks", price: 45.99, expiry: 365 }
+	];
 });
 
