ecc8a8328aba2a31594fb4c976a4b42b6d517954
Засыпин Николай <zasipin@mail.ru>
2014-10-26 00:17:09 +0400

    chapter 14 Using Custom Filters

--- controllers.js
+++ controllers.js
@@ -1,4 +1,4 @@
-var app = angular.module("exampleApp", []);
+var app = angular.module("exampleApp", ["customFilters"]);
 app.controller("defaultCtrl", function($scope){
 	$scope.products = [
 		{ name: "Apples", category: "Fruit", price: 1.20, expiry: 10 },
@@ -13,5 +13,22 @@ app.controller("defaultCtrl", function($scope){
 		{ name: "Wine", category: "Drinks", price: 8.99, expiry: 365 },
 		{ name: "Whiskey", category: "Drinks", price: 45.99, expiry: 365 }
 	];
+	$scope.getExpiryDate = function(days){
+		var now = new Date();
+		return now.setDate(now.getDate() + days);
+	};
+	$scope.selectItems = function(item){
+		return item.category == "Fish" || item.name == "Beer";
+	};
+	$scope.myCustomSorter = function(item){
+		return item.expiry < 5 ? 0 : item.price;
+	};
+	
+	$scope.limitVal = "5";
+	$scope.limitRange = [];
+	for (var i = (0 - $scope.products.length);
+		i <= $scope.products.length; i++) {
+		$scope.limitRange.push(i.toString());
+	}
 });
 
