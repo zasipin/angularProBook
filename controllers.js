ae69f7fa2e9c0bab39928e439570ccbb71b01215
Засыпин Николай <zasipin@mail.ru>
2014-10-25 20:32:45 +0400

    Working with other frameworks

--- controllers.js
+++ controllers.js
@@ -1,34 +1,16 @@
 var app = angular.module("exampleApp", []);
-app.controller("topLevelCtrl", function($scope){
-	$scope.data = {dataValue: "Hello Nick"};
-	$scope.reverseText = function(){
-		$scope.data.dataValue = $scope.data.dataValue.split("").reverse().join("");
+app.controller("simpleCtrl", function($scope){
+	$scope.buttonEnabled = true;
+	$scope.clickCounter = 0;
+	
+	$scope.handleClick = function(){
+		$scope.clickCounter++;
 	};
-	$scope.changeCase = function(){
-		var result = [];
-		angular.forEach($scope.data.dataValue.split(""), function (char, index){
-			result.push(index % 2 == 1
-				? char.toString().toUpperCase() : char.toString().toLowerCase());
+	$scope.$watch('buttonEnabled', function(newValue){
+		$('#jqui button').button({
+			disabled: !newValue
 		});
-		$scope.data.dataValue = result.join("");
-	};
+	});
+	
 });
 
-app.controller("firstChildCtrl", function($scope){
-	$scope.changeCase = function(){
-		$scope.data.dataValue = $scope.data.dataValue.toUpperCase();
-	};
-});
-
-app.controller("secondChildCtrl", function($scope){
-	$scope.changeCase = function(){
-		$scope.data.dataValue = $scope.data.dataValue.toLowerCase();
-	};
-	$scope.shiftFour = function(){
-		var result = [];
-		angular.forEach($scope.data.dataValue.split(""), function(char, index){
-			result.push(index < 4 ? char.toUpperCase() : char);
-		});
-		$scope.data.dataValue = result.join("");
-	};
-});
