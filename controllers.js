8ee6f8aa64a89e6ecc0f15c82107ba5c33898794
Засыпин Николай <zasipin@mail.ru>
2014-10-26 20:21:47 +0400

    chapter 15 Directives - custom directive - watch

--- controllers.js
+++ controllers.js
@@ -5,12 +5,37 @@ app.controller("defaultCtrl", function($scope){
 		{ name: "Bananas", category: "Fruit", price: 2.42, expiry: 7 },
 		{ name: "Pears", category: "Fruit", price: 2.02, expiry: 6 },
 	];
-	
+	$scope.incrementPrices = function () {
+		for (var i = 0; i < $scope.products.length; i++) {
+			$scope.products[i].price++;
+		}
+	}
 });
 
 app.directive("unorderedList", function(){
 	return function(scope, element, attrs){
-		
+		var data = scope[attrs["unorderedList"]],
+			propertyExpression = attrs["listProperty"];
+		if(angular.isArray(data)){
+			
+			var listElem = angular.element("<ul>");
+			element.append(listElem);
+			for(var i = 0; i < data.length; i++){
+				(function(){
+					var itemElement = angular.element('<li>');
+					listElem.append(itemElement);
+					var index  = i;
+					var watcherFn = function (watchScope) {
+						return watchScope.$eval(propertyExpression, data[index]);
+					}
+					scope.$watch(watcherFn, function (newValue, oldValue) {
+						itemElement.text(newValue);
+					});
+				}());
+				//listElem.append(angular.element("<li>").text(scope
+				//.$eval(propertyExpression, data[i])));
+			}
+		}
 	};
 });
 
