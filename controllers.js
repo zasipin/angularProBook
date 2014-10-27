a0a49bd4978861330f1c15330463675ab19aa3cc
Засыпин Николай <zasipin@mail.ru>
2014-10-27 21:57:22 +0400

    chapter 16 initialization

--- controllers.js
+++ controllers.js
@@ -17,23 +17,12 @@ app.directive("unorderedList", function(){
 		var data = scope[attrs["unorderedList"]],
 			propertyExpression = attrs["listProperty"];
 		if(angular.isArray(data)){
-			
 			var listElem = angular.element("<ul>");
 			element.append(listElem);
-			for(var i = 0; i < data.length; i++){
-				(function(){
-					var itemElement = angular.element('<li>');
-					listElem.append(itemElement);
-					var index  = i;
-					var watcherFn = function (watchScope) {
-						return watchScope.$eval(propertyExpression, data[index]);
-					}
-					scope.$watch(watcherFn, function (newValue, oldValue) {
-						itemElement.text(newValue);
-					});
-				}());
-				//listElem.append(angular.element("<li>").text(scope
-				//.$eval(propertyExpression, data[i])));
+			for (var i = 0; i < data.length; i++) {
+				var itemElement = angular.element("<li>")
+					.text(scope.$eval(propertyExpression, data[i]));
+				listElem.append(itemElement);
 			}
 		}
 	};
