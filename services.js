d80969b94b45e7709593f55f04d7720465064ccb
Засыпин Николай <zasipin@mail.ru>
2014-11-04 18:38:36 +0300

    chapter 18 - services

--- services.js
+++ services.js
@@ -1,10 +1,18 @@
+var baseLogger = function () {
+	this.messageCount = 0;
+	this.log = function (msg) {
+		console.log(this.msgType + ": " + (this.messageCount++) + " " + msg);
+	}
+};
+
+var debugLogger = function(){};
+debugLogger.prototype = new baseLogger();
+debugLogger.prototype.msgType = "Debug";
+
+var errorLogger = function () {};
+errorLogger.prototype = new baseLogger();
+errorLogger.prototype.msgType = "Error";
 
 angular.module("customServices", [])
-	.factory("logService", function(){
-		var messageCount = 0;
-		return {
-			log: function(msg){
-				console.log("(LOG + " + messageCount++ + ") " + msg);
-			}
-		};
-	});
+	.service("logService", debugLogger)
+	.service("errorService", errorLogger);
