0817705319ff590f872c761290b8118f7fed1a99
Засыпин Николай <zasipin@mail.ru>
2014-11-04 18:47:03 +0300

    chapter 18 - provider

--- services.js
+++ services.js
@@ -1,18 +1,35 @@
-var baseLogger = function () {
-	this.messageCount = 0;
-	this.log = function (msg) {
-		console.log(this.msgType + ": " + (this.messageCount++) + " " + msg);
-	}
-};
-
-var debugLogger = function(){};
-debugLogger.prototype = new baseLogger();
-debugLogger.prototype.msgType = "Debug";
-
-var errorLogger = function () {};
-errorLogger.prototype = new baseLogger();
-errorLogger.prototype.msgType = "Error";
-
 angular.module("customServices", [])
-	.service("logService", debugLogger)
-	.service("errorService", errorLogger);
+	.provider("logService", function(){
+		var counter = true,
+			debug = true;
+		return {
+			messageCounterEnabled: function(setting){
+				if(angular.isDefined(setting)){
+					counter = setting;
+					return this;
+				} else {
+					return counter;
+				}
+			},
+			debugEnabled: function(setting){
+				if(angular.isDefined(setting)){
+					debug = setting;
+					return this;
+				} else {
+					return debug;
+				}
+			},
+			$get: function(){
+				return {
+					messageCount: 0,
+					log: function (msg) {
+						if(debug){
+							console.log("(LOG + " 
+								+ (counter ? " + " + this.messageCount++ + ") " : ") ")
+							 	+ msg);	
+						}
+					}						
+				};
+			}
+		};		
+	});
