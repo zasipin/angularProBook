da8ad084adc6319a4881c817f0f0d6c6311c9efa
Засыпин Николай <zasipin@mail.ru>
2014-11-04 18:05:14 +0300

    chapter 18 - services an modules

---
+++ services.js
@@ -0,0 +1,10 @@
+
+angular.module("customServices", [])
+	.factory("logService", function(){
+		var messageCount = 0;
+		return {
+			log: function(msg){
+				console.log("(LOG + " + messageCount++ + ") " + msg);
+			}
+		};
+	});
