d675397abe571d1b09675351e1a624787877ab1f
zasipin <zasipin.it@gmail.com>
2014-12-20 14:51:31 +0300

    testing filters, directives

--- app.js
+++ app.js
@@ -21,4 +21,28 @@ angular.module("exampleApp", [])
 		$scope.incrementCounter = function(){
 			$scope.counter++;
 		};
+	})
+	.filter("labelCase", function(){
+		return function(value, reverse){
+			if(angular.isString(value)){
+				var intermediate = reverse ? value.toUpperCase() : value.toLowerCase();
+				return (reverse ? intermediate[0].toLowerCase() 
+					: intermediate[0].toUpperCase()) + intermediate.substr(1);
+			} else {
+				return value;
+			}
+		};
+	})
+	.directive("unorderedList", function(){
+		return function(scope, element, attrs) {
+			var data = scope[attrs["unorderedList"]];
+			if(angular.isArray(data))
+			{
+				var listElem = angular.element("<ul>");
+				element.append(listElem);
+				for (var i = 0; i < data.length; i++) {
+					listElem.append(angular.element("<li>").text(data[i].name));
+				};
+			}
+		};
 	});
