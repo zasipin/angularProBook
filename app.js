8eb21dddd896ebbca0c9115ac4fd340557a0efb8
zasipin <zasipin.it@gmail.com>
2014-12-20 13:00:14 +0300

    mock $httpBackend

--- app.js
+++ app.js
@@ -1,5 +1,9 @@
 angular.module("exampleApp", [])
-	.controller("defaultCtrl", function($scope){
+	.controller("defaultCtrl", function($scope, $http){
+		$http.get("productData.json")
+			.success(function(data){
+				$scope.products = data;
+			});
 		$scope.counter = 0;
 		$scope.incrementCounter = function(){
 			$scope.counter++;
