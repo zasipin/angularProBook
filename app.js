2cbecc30144067d3d0efd5be1bc63da95db14de4
zasipin <zasipin.it@gmail.com>
2014-12-22 20:45:43 +0300

    service tests

--- app.js
+++ app.js
@@ -45,4 +45,15 @@ angular.module("exampleApp", [])
 				};
 			}
 		};
+	})
+	.factory("counterService", function(){
+		var counter = 0;
+		return {
+			incrementCounter: function(){
+				counter++;
+			},
+			getCounter: function(){
+				return counter;
+			}
+		}
 	});
